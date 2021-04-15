ab47594983b4a1b123deeef7b0d5aa8c0d6ec474
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-14 22:33:41 -0300

    Configure pgsql database

--- src/pycarbonara/settings.py
+++ src/pycarbonara/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "pycarbonara.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "HOST": "db",
+        "PASSWORD": "pycarbonara",
+        "PORT": 5432,
     }
 }
 
