4aec2d92e36964911cc1ac68133ff8f0002ef901
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-19 23:13:45 -0300

    update views

--- src/carbon/views.py
+++ src/carbon/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from urllib.parse import urlencode
 
 from .serializers import OptionsSerializer
 
@@ -10,7 +11,8 @@ class CarbonView(APIView):
     def post(self, request):
         serializers = OptionsSerializer(data=request.data)
         if serializers.is_valid():
-            return Response({"status": serializers.data})
+            url_pronta = urlencode(serializers.data)
+            return Response({"status": "https://carbon.now.sh/?" + url_pronta})
         return Response(
             {"message": serializers.errors}, status=status.HTTP_400_BAD_REQUEST
         )
