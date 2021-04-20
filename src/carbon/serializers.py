b5f489bff42a658d70be017c2530efa8b42890fb
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-19 22:58:22 -0300

    Add options serializer

---
+++ src/carbon/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+
+class OptionsSerializer(serializers.Serializer):
+    code = serializers.CharField(required=True)  # Code
+    bg = serializers.CharField(default="rgba(171, 184, 195, 1)")  # Background
+    ds = serializers.BooleanField(default=True)  # dropShadow
+    dsblur = serializers.CharField(default="68px")  # dropShadowBlurRadius
+    dsyoff = serializers.CharField(default="20px")  # dropShadowOffsetY
+    es = serializers.CharField(default="2x")  # exportSize
+    fm = serializers.CharField(default="Hack")  # fontFamily
+    fl = serializers.IntegerField(default=1)  # firstLineNumber
+    fs = serializers.CharField(default="14px")  # fontSize
+    l = serializers.CharField(default="auto")  # language
+    lh = serializers.CharField(default="133%")  # lineHeight
+    ln = serializers.BooleanField(default=False)  # lineNumbers
+    ph = serializers.CharField(default="56px")  # paddingHorizontal
+    pv = serializers.CharField(default="56px")  # paddingVertical
+    si = serializers.BooleanField(default=False)  # squaredImage
+    t = serializers.CharField(default="seti")  # theme
+    wm = serializers.BooleanField(default=False)  # watermark
+    wa = serializers.BooleanField(default=True)  # widthAdjustment
+    wc = serializers.BooleanField(default=True)  # windowControls
+    wt = serializers.CharField(default="none")  # windowTheme
--- src/carbon/views.py
+++ src/carbon/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import OptionsSerializer
+
 # Create your views here.
 class CarbonView(APIView):
     def post(self, request):
-        return Response({"status": "OK"})
+        serializers = OptionsSerializer(request.data)
+        return Response({"status": serializers})
