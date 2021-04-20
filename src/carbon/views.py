3f829a2748630266e82a1f5e4b7ff74093303d11
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-19 23:03:47 -0300

    update views

--- src/carbon/views.py
+++ src/carbon/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import OptionsSerializer
 
 # Create your views here.
 class CarbonView(APIView):
     def post(self, request):
-        serializers = OptionsSerializer(request.data)
-        return Response({"status": serializers})
+        serializers = OptionsSerializer(data=request.data)
+        if serializers.is_valid():
+            return Response({"status": serializers.data})
+        return Response(
+            {"message": serializers.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
