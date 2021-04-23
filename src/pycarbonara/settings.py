336ec8edfc86ff3a1b4cbf7c2c1c60a229084659
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-23 00:56:09 -0300

    add dj_database_url

--- src/pycarbonara/settings.py
+++ src/pycarbonara/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,6 +101,8 @@ DATABASES = {
         "PORT": os.environ.get("DATABASE_PORT", 5432),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
