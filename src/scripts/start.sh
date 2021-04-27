cec5efa296303edd375fb78049bdb4d308dc6ebe
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-27 01:03:24 -0300

    Remove workers

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -12,5 +12,5 @@ elif [[ ${ENVIRONMENT} == "production" ]]; then
     pyppeteer-install
     python manage.py migrate
     python manage.py collectstatic --noinput --clear
-    gunicorn pycarbonara.wsgi --bind 0.0.0.0:$PORT --timeout 120 --workers 20
+    gunicorn pycarbonara.wsgi --bind 0.0.0.0:$PORT --timeout 120
 fi
