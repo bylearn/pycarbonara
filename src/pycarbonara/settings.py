279397212faf569d25edd66d288891f5e3d07542
Guilherme Haynes Howe <zerossb@gmail.com>
2021-04-27 00:04:22 -0300

    add white noise to production

--- src/pycarbonara/settings.py
+++ src/pycarbonara/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "pycarbonara.urls"
@@ -145,9 +146,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/pycarbonara"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/media/")
+MEDIA_ROOT = os.path.join(STATIC_ROOT, "/media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
