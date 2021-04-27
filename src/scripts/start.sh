a54ed2de8a7e4fddec98b7472731d5accc7891ec
Felipe Cabrera <felipe@bylearn.com.br>
2021-04-27 04:12:17 -0300

    Disabling Workers Connection (Digital Ocean Limitation)

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -12,5 +12,5 @@ elif [[ ${ENVIRONMENT} == "production" ]]; then
     pyppeteer-install
     python manage.py migrate
     python manage.py collectstatic --noinput --clear
-    gunicorn pycarbonara.wsgi --bind 0.0.0.0:$PORT --timeout 120 --worker-class=gevent --worker-connections=1000 --workers=3
+    gunicorn pycarbonara.wsgi --bind 0.0.0.0:$PORT --timeout 120 --workers=3
 fi
